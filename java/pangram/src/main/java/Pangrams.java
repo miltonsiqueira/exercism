43faf53df1193853e2c86d6b16377ea70781a7a7
Milton Siqueira <miltonsiqueira@gmail.com>
2016-06-08 16:14:51 +0100

    Pangram - remove toLowerCase

--- java/pangram/src/main/java/Pangrams.java
+++ java/pangram/src/main/java/Pangrams.java
@@ -1,13 +1,13 @@
 
 public class Pangrams{
     public static boolean isPangram(String s){
+
         if(s == null || s.isEmpty()){
           return false;
         }
 
-        s = s.toLowerCase();
-        for(char c = 'a'; c <= 'z'; c++){
-            if(s.indexOf(c) < 0){
+        for(char c = 'A'; c <= 'Z'; c++){
+            if(s.indexOf(c) < 0 && s.indexOf((char)(c + 32)) < 0 ){
                 return false;
             }
         }
